74ecc00a51a69ad4542091076740754a844c3e5f
jorge <jmaciasvesga@gmail.com>
2021-01-20 07:44:32 -0500

    com

---
+++ build/precache-manifest.62ba129af368f5413cc68d568a37bb14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c14847b9f4ed9b4edef175a8ad41b5",
+    "url": "/filmes_app/index.html"
+  },
+  {
+    "revision": "83710f608ac8be945215",
+    "url": "/filmes_app/static/css/main.721ac0a3.chunk.css"
+  },
+  {
+    "revision": "8236cc8256839a771968",
+    "url": "/filmes_app/static/js/2.8bbf497b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/filmes_app/static/js/2.8bbf497b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83710f608ac8be945215",
+    "url": "/filmes_app/static/js/main.29fa931a.chunk.js"
+  },
+  {
+    "revision": "aa342eb5e021d3c9f113",
+    "url": "/filmes_app/static/js/runtime-main.bb389325.js"
+  }
+]);
